21ab1aba3dd85776fb12356af8ff2c571e878e39
surevmaya <SUREV@HOTMAIL.COM>
2026-05-15 07:13:38 -0700

    Update tools.py

--- 02-customer-sentiment-agent/tools.py
+++ 02-customer-sentiment-agent/tools.py
@@ -1,9 +1,38 @@
-def get_movie_info(movie_title: str) -> str:
-    """Get movie info and ratings from OMDB/IMDB"""
-    api_key = os.environ.get("OMDB_API_KEY")
-    
-    url = "http://www.omdbapi.com/"
-    params = {
-        "t": "Citadel"        # movie title
-        "apikey": ???    # your key
+TOOLS = [
+    {
+        "name": "search_vendor_news",
+        "description": "Search recent news about SAP including financial health, stock performance, legal issues and competitor activity",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "vendor": {
+                    "type": "string",
+                    "description": "Name of the software vendor SAP"
+                },
+                "product": {
+                    "type": "string",
+                    "description": "Name of the product S/4HANA"
+                }
+            },
+            "required": ["vendor", "product"]
+        }
+    },
+    {
+        "name": "search_vendor_reviews",
+        "description": "Search for customer reviews and user experiences including complaints, frustrations and satisfaction levels",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "vendor": {
+                    "type": "string",
+                    "description": "Name of the software vendor SAP"
+                },
+                "product": {
+                    "type": "string",
+                    "description": "Name of the product S/4HANA"
+                }
+            },
+            "required": ["vendor", "product"]
+        }
     }
+]
